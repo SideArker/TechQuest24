9f70138bd5ed8017528250046f69c94905944721
Radosław Marek <dotims054@gmail.com>
2024-04-19 09:02:49 +0200

    header with responsiveness

--- i-canteen-front/src/components/header/index.tsx
+++ i-canteen-front/src/components/header/index.tsx
@@ -2,46 +2,12 @@ import React, { useState } from 'react';
 import  Hamburger  from 'hamburger-react';
 
 const Header = () => {
-
+  const [isOpen, setOpen] = useState(false);
   return (
-    <header className='bg-[#10386b]'>
-      <nav className='flex item-center justify-center'>
-          <div>
-            <span className='text-extrabold text-white text-xl'>MCX STUDIO</span>
-          </div>
-
-          <div className='block md:hidden'>
-              <button>
-                <Hamburger></Hamburger>
-              </button>
-          </div>
-      </nav>
-    </header>
-  );
-}
-
-export default Header;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-<header className="bg-[#183c6c]  p-5">
+    <header className="bg-[#183c6c]  p-5">
       <nav className="max-w-6xl mx-auto flex items-center justify-between flex-wrap">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <span className="font-semibold text-xl tracking-tight">MCX STUDIO</span>
+          <span className="font-semibold text-xl tracking-tight">JAJO SKUAŁD</span>
         </div>
         <div className="block md:hidden">
           <button className="text-white focus:outline-none">
@@ -51,16 +17,19 @@ export default Header;
         <div className={`w-full ${isOpen ? 'block' : 'hidden'} flex-grow md:flex md:items-center justify-end md:w-auto`}>
           <div className="text-md font-bold">
             <a href="#home" className="block mt-4 md:inline-block md:mt-0 text-white mr-4 px-5 py-3">
-              Home
+              Strona główna
             </a>
-            <a href="#about" className="block mt-4 md:inline-block md:mt-0 text-white mr-4 px-5 py-3">
-              About
+            <a href="#stolowka" className="block mt-4 md:inline-block md:mt-0 text-white mr-4 px-5 py-3">
+              Stołówka
             </a>
-            <a href="#contact" className="block mt-4 md:inline-block md:mt-0 text-white px-5 py-3">
-              Contact
+            <a href="#Opcje" className="block mt-4 md:inline-block md:mt-0 text-white px-5 py-3">
+              Opcje
             </a>
           </div>
         </div>
       </nav>
     </header>
-    */
+  );
+}
+
+export default Header;
